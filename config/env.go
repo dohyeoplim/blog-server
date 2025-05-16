b5faba5209d33d534d5c4f496cbdae302586cd27
Dohyeop Lim <dhlim0817@gmail.com>
2025-05-17 04:18:18 +0900

    fix: railway env support

--- config/env.go
+++ config/env.go
@@ -1,11 +1,18 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	if err := godotenv.Load(); err != nil {
-		panic("Error loading .env file")
+	if os.Getenv("RAILWAY_ENVIRONMENT_NAME") == "production" {
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("No .env file found. Using system environment variables.")
+		} else {
+			fmt.Println(".env file loaded successfully.")
+		}
 	}
 }
