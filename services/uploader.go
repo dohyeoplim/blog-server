fecf99abe189aa7578d49f78d27ba03f80d9579e
Dohyeop Lim <dhlim0817@gmail.com>
2025-05-17 17:15:38 +0900

    add temporal debug codes

--- services/uploader.go
+++ services/uploader.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"log"
 	"mime/multipart"
 	"os"
 
@@ -11,23 +12,32 @@ import (
 )
 
 func UploadToR2(file multipart.File, filename string) (string, error) {
+	log.Println("📤 Starting R2 upload...")
 	client := NewR2Client()
 
-	_, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket:      aws.String(os.Getenv("R2_BUCKET_NAME")),
+	bucket := os.Getenv("R2_BUCKET_NAME")
+	accountID := os.Getenv("R2_ACCOUNT_ID")
+
+	log.Printf("🪣 Bucket: %s\n", bucket)
+	log.Printf("👤 Account ID: %s\n", accountID)
+	log.Printf("📁 Filename: %s\n", filename)
+
+	input := &s3.PutObjectInput{
+		Bucket:      aws.String(bucket),
 		Key:         aws.String(filename),
 		Body:        file,
 		ContentType: aws.String("image/jpeg"),
-	})
+	}
+
+	log.Println("📡 Sending PutObject request to R2...")
+	_, err := client.PutObject(context.TODO(), input)
 	if err != nil {
+		log.Printf("❌ Failed to upload to R2: %v\n", err)
 		return "", err
 	}
 
-	url := fmt.Sprintf("https://%s.r2.cloudflarestorage.com/%s/%s",
-		os.Getenv("R2_ACCOUNT_ID"),
-		os.Getenv("R2_BUCKET_NAME"),
-		filename,
-	)
+	url := fmt.Sprintf("https://%s.r2.cloudflarestorage.com/%s/%s", accountID, bucket, filename)
+	log.Printf("✅ Upload successful. URL: %s\n", url)
 
 	return url, nil
 }
